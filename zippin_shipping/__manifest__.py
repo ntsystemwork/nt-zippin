7071133f5e350b5af0517601c6f935dc528e512c
faf1997 <alejandrofranfio@gmail.com>
2024-10-30 00:57:40 +0000

    [UPD] zippin

--- zippin_shipping/__manifest__.py
+++ zippin_shipping/__manifest__.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "NT Zippin Odoo Connector",
+    'name': "Envíos Zippin",
 
     'summary': """
-        Connector Odoo-Zippin for Shipping Payment""",
+        permite hacer envíos y consultar tarifas desde los presupuestos en odoo""", 
 
     'description': """
-        Connector Odoo-Zippin for Shipping Payment
+        para instalar el módulo debe agregar el repositorio 
+        https://github.com/OCA/product-attribute.git rama 16.0
+    
+        1) para instalar el modulo debe crear una cuenta en zippin
+        2) crear las credenciales en la cuenta de zippin
+        3) agregar las credenciales en odoo
     """,
 
     'author': "hitofusion",
     'website': "https://www.hitofusion.com",
     'category': 'Sales',
 
-    'version': '16.0.1.2.5',
+    'version': '16.0.1.2.6',
     'depends': [
         'base', 
         'account',
@@ -24,19 +29,6 @@
         'website_sale',
         'l10n_ar'
         ],
-
-    # 'data': [
-    #     'wizard/choose_delivery_carrier_form_view.xml', XXXXXXXX
-    #     'security/ir.model.access.csv', xxxxxxx
-    #     'data/zippin_data.xml', xxxxxxx
-    
-    #     'views/sale_view.xml',
-    
-    #     'views/account_move_view.xml',
-    #     'views/stock_picking_view.xml',
-    #     'reports/estimated_delivery_report.xml',
-    #     'views/website_sale_view.xml',
-    # ],
     
     'data': [
         'data/zippin_data.xml',
@@ -55,7 +47,7 @@
     'assets': {
         'web.assets_frontend': [
             'zippin/static/src/js/zippin_odoo.js',
-            #'zippin/static/src/scss/zippin_odoo.scss'
+            'zippin/static/src/scss/zippin_odoo.scss'
         ],
     },
 }
